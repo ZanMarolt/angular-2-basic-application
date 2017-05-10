47e1e5c7b9d7b84d28fe19e75477fe4f915e7d6c
Zan Marolt <marolt.zan@hotmail.com>
2017-05-10 17:13:17 +0200

    Simple directive

--- app/courses.component.ts
+++ app/courses.component.ts
@@ -3,20 +3,22 @@
  */
 import {Component} from 'angular2/core'
 import {CourseService} from './course.service'
-
+import {AutoGrowDirective} from "./auto-grow.directive";
 
 @Component({
     selector: 'courses',
     template: `
         <h2>Courses</h2>
         {{ title }}
+        <input autoGrow />
         <ul>
             <li *ngFor="#course of courses">
                 {{ course }}
             </li>
         </ul>
     `,
-    providers: [CourseService]
+    providers: [CourseService],
+    directives: [AutoGrowDirective]
 })
 
 export class CoursesComponent {
